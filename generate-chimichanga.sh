f89205954447c5df055333bea33831984c8249b4
Isa Farnik <isa@konghq.com>
2022-11-30 16:43:35 -0800

    wip

--- generate-chimichanga.sh
+++ generate-chimichanga.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 function main() {
+    # shellcheck source=src/tortilla.inc
+    source src/tortilla.inc
+
     echo -e "_ ${YAY} _" > chimichanga
     chmod a+x chimichanga
+
 }
 
 main
